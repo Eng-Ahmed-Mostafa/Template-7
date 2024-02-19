b871c96d059f9223e255b7a4ad9b1182bbc47ca7
Eng-Ahmed-Mostafa <ahmed.hash.developer@gmail.com>
2024-02-19 10:07:49 +0200

    CREAT Site up

---
+++ js/main.js
@@ -0,0 +1,37 @@
+let bar = document.querySelector(".bar");
+window.onscroll = function () {
+    if(window.scrollY >= window.innerHeight - 63) {
+        console.log(window.scrollY)
+        bar.classList.add("display");
+    }
+    else {
+        bar.classList.remove("display");
+    }
+}
+let displayItems = document.querySelectorAll(".show-display");
+window.onload = function() {
+    displayItems.forEach(e => {
+        e.classList.add("display")
+    });
+}
+let contentMenu = document.querySelector(".content-menu");
+let removeMenu = document.querySelector(".content-menu i");
+removeMenu.onclick = function() {
+    contentMenu.classList.add("no-display");
+}
+let displayMenu = document.querySelectorAll(".bar .menu > *");
+displayMenu.forEach(e => {
+    e.onclick = function() {
+        contentMenu.classList.remove("no-display")
+    }
+})
+let transformSection = document.querySelectorAll(".landing .transform-section > *");
+transformSection.forEach(e => {
+    e.onclick = function() {
+        window.scrollTo({
+            left: 0,
+            top: window.innerHeight - 63,
+            behavior: "smooth"
+        });
+    }
+})
